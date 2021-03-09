4228dcabc657288c0d9b54b52a30202825b322a0
Talha Khan <talha.k2625@gmail.com>
2021-03-10 01:04:50 +0500

    init 1st section

---
+++ src/components/SectionThree.js
@@ -0,0 +1,30 @@
+import { Container, Row, Col } from 'reactstrap';
+
+import one from '../assets/images/1.png';
+import two from '../assets/images/2.png';
+import three from '../assets/images/3.png';
+import four from '../assets/images/4.png';
+import five from '../assets/images/5.png';
+
+function SectionThree() {
+  return (
+    <Container className="mt-5">
+      <Row className="text-center">
+        <Col>
+          <h2 className="s-h">How We Build The Apps?</h2>
+        </Col>
+      </Row>
+      <Row className="text-center mt-4">
+        <Col md="3">
+          <img src={one} />
+          <div className="lh-m">
+            <p className="s-t mt-3 lh-p"><b>Requirement Gathering <br/> / Analysis</b></p>
+            <small>Requirement Gathering <br/> / Analysis</small>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default SectionThree;
---
+++ src/components/SectionTwo.js
@@ -0,0 +1,41 @@
+import { Container, Row, Col } from 'reactstrap';
+
+import ios from '../assets/images/ios.png';
+import android from '../assets/images/android.png';
+import web from '../assets/images/web.png';
+import game from '../assets/images/game.png';
+
+function SectionTwo() {
+  return (
+    <Container>
+      <Row className="text-center">
+        <Col>
+          <h2 className="s-h">What We Do</h2>
+          <div className="s-d">
+            <p className="s-p mt-3">We eat, sleep and breathe. It's who we are. We create the kind of apps that people love & are used over & over again.</p>
+          </div>
+        </Col>
+      </Row>
+      <Row className="text-center mt-4">
+        <Col md="3">
+          <img src={ios} />
+          <p className="s-t mt-3">IOS <br/> Development</p>
+        </Col>
+        <Col md="3">
+          <img src={android} />
+          <p className="s-t mt-3">Android <br/> Development</p>
+        </Col>
+        <Col md="3">
+          <img src={game} />
+          <p className="s-t mt-3">Game <br/> Development</p>
+        </Col>
+        <Col md="3">
+          <img src={web} />
+          <p className="s-t mt-3">Web <br/> Development</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default SectionTwo;
